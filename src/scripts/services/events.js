d18f436332aa2e8d03f05bb496bb86c2582f979a
Lucas Lourenço <lucas.dev.profissional2001@gmail.com>
2025-09-15 09:06:01 -0300

    proxy

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -1,9 +1,16 @@
+async function getEvents(userName) {
+  
+  const githubUrl = `https://api.github.com/users/${userName}/events`;
+  
+  const proxy = 'https://api.allorigins.win/raw?url=';
 
+  const response = await fetch(proxy + encodeURIComponent(githubUrl));
 
-async function getEvents(userName){
-    let response = await fetch(`https://api.github.com/users/${userName}/events`);
+  if (!response.ok) {
+    throw new Error("Erro ao buscar eventos");
+  }
 
-    return await response.json()
+  return await response.json();
 }
 
-export {getEvents}
+export { getEvents };
